e78ebf8c209b95d364fd4caf4bfe0b4c54e17364
capouch <brianc@palaver.net>
2015-12-08 03:30:41 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Set up server, hook on processors for websockets, files, and Mongo
+var  http    = require( 'http'                 ),
+  express = require( 'express'              ),
+
+  app     = express();
+
+app.use( express.static( __dirname + '/' ) );
+
+app.listen(8000);
+
