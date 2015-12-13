4b6a8a30c6c88d4e3f7842469304fc91edac3720
capouch <brianc@palaver.net>
2015-12-13 16:36:10 -0500

    Clean up server file

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 // Set up server, hook on processors for websockets, files, and Mongo
 var  http    = require( 'http'                 ),
-  express = require( 'express'              ),
+     express = require( 'express'              ),
 
-  app     = express();
-
+app     = express();
 app.use( express.static( __dirname + '/' ) );
-
 app.listen(3001);
