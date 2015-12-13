39961f7c92b4e82b68ce4b5a68d17560f927d2cf
capouch <brianc@palaver.net>
2015-12-13 16:49:40 -0500

    Clean up and comment converter script

--- sharpConvert.js
+++ sharpConvert.js
@@ -1,33 +1,33 @@
 
 //  sharpConvert.js: Convert an image file into Deep Zoom format
-//     Brian Capouch 9 December 2015
+//     Brian Capouch 13 December 2015
 //     This version prompts for names of image file and tiles base
+//       if the user does not provide them via the CL
 //     Output: .dzi file and directory of tile images
 //
 
-// To-do: allow command-line input of values
-
 // Set up functionality
-var sharp = require('sharp'),
-  prompt = require('prompt'),
-  optimist = require('optimist');
+var sharp    = require('sharp'),
+    prompt   = require('prompt'),
+    optimist = require('optimist');
 
-  // Set overrides from command line
-  prompt.override = optimist.argv;
+// Set overrides from command line
+prompt.override = optimist.argv;
 
 // Get ready to talk to the user
 prompt.start();
 
+// Get input from user (or use argv values provide via override)
 prompt.get(['filename', 'dziname'], function (err, result) {
   if (err) { return onErr(err); }
   console.log('Command-line input received:');
   console.log('Filename: ' + result.filename);
   console.log('Dziname: ' + result.dziname);
-  var infile = result.filename,
-    dzibase = result.dziname + '.dzi';
+  var infile  = result.filename,
+      dzibase = result.dziname + '.dzi';
 
-  // Perform conversion
-  sharp(infile).tile(256).toFile(dzibase, function(err, info) {
+// Perform conversion
+sharp(infile).tile(256).toFile(dzibase, function(err, info) {
   });
 });
 
@@ -35,9 +35,3 @@ function onErr(err) {
   console.log(err);
   return 1;
 }
-
-
-//sharp(infile).tile(256).toFile(outfile, function(err, info) {
-  // The output.dzi file is the XML format Deep Zoom definition
-  // The output_files directory contains 256x256 pixel tiles grouped by zoom level
-//});
